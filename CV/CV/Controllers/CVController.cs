3fc536d746e01800284641e976255f3e941493bb
ahasanul <ahasanulislam973@gmail.com>
2022-05-30 00:45:06 +0600

    Lab Task1

---
+++ CV/CV/Controllers/CVController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class CVController : Controller
+    {
+        //
+        // GET: /CV/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+
+        public ActionResult EducationalQualification()
+        {
+            return View();
+        }
+
+        public ActionResult ContactInfo()
+        {
+            return View();
+        }
+
+    }
+}
